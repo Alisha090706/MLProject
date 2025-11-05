00765c3ce0d7ee343e23992886be490283589783
Alisha090706 <alishakapoor0907@gmail.com>
2025-11-05 16:30:18 +0530

    Model Training and HyperParameter Tuning Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTranformationConfig
 from src.components.data_transformation import DataTranformer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -47,4 +49,7 @@ if __name__=='__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTranformer()
-    data_transformation.initiate_data_transformation(train_path=train_data,test_path=test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_path=train_data,test_path=test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr=train_arr,test_arr=test_arr))
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -10,5 +12,27 @@ def save_object(file_path,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,"wb") as file:
             dill.dump(obj,file)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(X_train,X_test,y_train,y_test,models,param):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            rs=RandomizedSearchCV(model,para,cv=3,n_jobs=-1)
+            # model.fit(X_train,y_train)
+            rs.fit(X_train,y_train)
+            model.set_params(**rs.best_params_)
+            model.fit(X_train,y_train)
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            train_model_score=r2_score(y_true=y_train,y_pred=y_train_pred)
+            test_model_score=r2_score(y_pred=y_test_pred,y_true=y_test)
+
+            report[list(models.keys())[i]]=test_model_score
+        return report
     except Exception as e:
         raise CustomException(e,sys)
